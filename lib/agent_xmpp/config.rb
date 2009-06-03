8cfece09e099876dcd31f4b5d039fa63ea438a8d
Troy Stribling <troy.stribling@gmail.com>
2009-06-03 08:14:41 -0400

    starting work in test cases

---
+++ lib/agent_xmpp/config.rb
@@ -0,0 +1,14 @@
+module AgentXmpp
+
+  VERSION = "0.0.0"
+  AGENT_XMPP_NAME = 'AgentXMPP'
+  OS_VERSION = IO.popen('uname -sr').readlines.to_s.strip
+  @config_file = "config/agent_xmpp.yml"
+  @app_dir = "."
+
+  class << self
+    attr_accessor :config_file, :app_dir
+  end
+  
+end
+
