58936745995e0e67c43011ba8f82b393154a36bb
Troy Stribling <troy.stribling@gmail.com>
2009-06-16 01:20:46 -0400

    a littel test caes refactoring

--- test/test_cases/test_client_version_discovery.rb
+++ test/test_cases/test_client_version_discovery.rb
@@ -5,27 +5,28 @@ require 'test_helper'
 class TestClientVersionDiscovery < Test::Unit::TestCase
 
   #.........................................................................................................
-  def get_client_version
-    @client.receiving(PresenceMessages.recv_presence_available(@client, 'troy@nowhere.com/home')).should \
-      respond_with(SystemDiscoveryMessages.send_client_version_get(@client, 'troy@nowhere.com/home'))
-    @client.receiving(SystemDiscoveryMessages.recv_client_version_result(@client, 'troy@nowhere.com/home')).should not_respond
-  end  
-
+  def setup
+    @config = {'jid' => 'test@nowhere.com', 'contacts' =>['dev@nowhere.com', 'troy@nowhere.com'], 'password' => 'nopass'}
+    @client = TestClient.new(@config)
+    test_init_roster(@client, @config)
+    @delegate = @client.new_delegate
+  end
+  
   ####-------------------------------------------------------------------------------------------------------
-  context "a specified client configuration" do
+  context "on receiving a presence message of type=available from a jid in configured roster" do
   
     #.........................................................................................................
     setup do
-      @config = {'jid' => 'test@nowhere.com', 'contacts' =>['dev@nowhere.com', 'troy@nowhere.com'], 'password' => 'nopass'}
-      @client = TestClient.new(@config)
-      test_init_roster(@client, @config)
-      @delegate = @client.new_delegate
-    end
-    
-    #.........................................................................................................
-    should "send a client version request upon receiving a contact presence message with type=available from a jid and add information to roster item " do
+      @delegate.did_receive_client_version_request_method.should_not be_called
       @delegate.did_receive_client_version_result_method.should_not be_called
-      get_client_version
+      @client.receiving(PresenceMessages.recv_presence_available(@client, 'troy@nowhere.com/home')).should \
+        respond_with(SystemDiscoveryMessages.send_client_version_get(@client, 'troy@nowhere.com/home'))
+      @client.receiving(SystemDiscoveryMessages.recv_client_version_result(@client, 'troy@nowhere.com/home')).should not_respond
+      @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'].should_not be_nil
+    end  
+  
+    #.........................................................................................................
+    should "send a client version request to that jid and add result version information to roster item " do
       @client.receiving(SystemDiscoveryMessages.recv_client_version_result(@client, 'troy@nowhere.com/home')).should not_respond
       @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'][:version].should_not be_nil
       @delegate.did_receive_client_version_result_method.should be_called
@@ -33,24 +34,21 @@ class TestClientVersionDiscovery < Test::Unit::TestCase
     end
     
     #.........................................................................................................
-    should "respond to client version requests from jids in configured roster" do
-      @delegate.did_receive_client_version_request_method.should_not be_called
-      get_client_version
-      @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'].should_not be_nil
+    should "respond to client version requests from that jid" do
       @client.receiving(SystemDiscoveryMessages.recv_client_version_get(@client, 'troy@nowhere.com/home')).should \
         respond_with(SystemDiscoveryMessages.send_client_version_result(@client, 'troy@nowhere.com/home'))
       @delegate.did_receive_client_version_request_method.should be_called
     end
-    
-    #.........................................................................................................
-    should "not respond to client version requests from jids not in configured roster" do
-      @delegate.did_receive_client_version_result_method.should_not be_called
-      @client.roster.has_key?('noone@nowhere.com').should be(false)
-      @client.receiving(SystemDiscoveryMessages.recv_client_version_get(@client, 'noone@nowhere.com/nothing')).should not_respond
-      @delegate.did_receive_client_version_request_method.should be_called
-    end
-    
-  end
 
+  end
+    
+  #.........................................................................................................
+  should "not respond to client version requests from jids not in configured roster" do
+    @delegate.did_receive_client_version_result_method.should_not be_called
+    @client.roster.has_key?('noone@nowhere.com').should be(false)
+    @client.receiving(SystemDiscoveryMessages.recv_client_version_get(@client, 'noone@nowhere.com/nothing')).should not_respond
+    @delegate.did_receive_client_version_request_method.should be_called
+  end
+    
 end
 
--- test/test_cases/test_presence_management.rb
+++ test/test_cases/test_presence_management.rb
@@ -4,43 +4,32 @@ require 'test_helper'
 ##############################################################################################################
 class TestPresenceManagement < Test::Unit::TestCase
 
-  def first_presence
-    @client.roster['troy@nowhere.com'][:resources].should be_empty
-    @delegate.did_receive_presence_method.should_not be_called
-    @client.receiving(PresenceMessages.recv_presence_available(@client, 'troy@nowhere.com/home')).should \
-      respond_with(SystemDiscoveryMessages.send_client_version_get(@client, 'troy@nowhere.com/home'))
-    @delegate.did_receive_presence_method.should be_called
-    @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'][:presence].should_not be_nil
+  #.........................................................................................................
+  def setup
+    @config = {'jid' => 'test@nowhere.com', 'contacts' =>['dev@nowhere.com', 'troy@nowhere.com'], 'password' => 'nopass'}
+    @client = TestClient.new(@config)
+    test_init_roster(@client, @config)
+    @delegate = @client.new_delegate
   end
-  
+    
   ####------------------------------------------------------------------------------------------------------
-  context "a specified client configuration" do
+  context "on receipt of first presence message from jid in configured roster" do
+
+    setup do
+      @client.roster['troy@nowhere.com'][:resources].should be_empty
+      @delegate.did_receive_presence_method.should_not be_called
+      @client.receiving(PresenceMessages.recv_presence_available(@client, 'troy@nowhere.com/home')).should \
+        respond_with(SystemDiscoveryMessages.send_client_version_get(@client, 'troy@nowhere.com/home'))
+      @delegate.did_receive_presence_method.should be_called
+      @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'][:presence].should_not be_nil
+    end
   
     #.........................................................................................................
-    setup do
-      @config = {'jid' => 'test@nowhere.com', 'contacts' =>['dev@nowhere.com', 'troy@nowhere.com'], 'password' => 'nopass'}
-      @client = TestClient.new(@config)
-      test_init_roster(@client, @config)
-      @delegate = @client.new_delegate
-    end
-    
-    #.........................................................................................................
-    should "create presence status for resource on receipt of self presence" do
-      @client.roster[@client.client.jid.bare.to_s][:resources].should be_empty
-      @delegate.did_receive_presence_method.should_not be_called
-      @client.receiving(PresenceMessages.recv_presence_self(@client)).should not_respond
-      @delegate.did_receive_presence_method.should be_called
-      @client.roster[@client.client.jid.bare.to_s][:resources][@client.client.jid.to_s][:presence].should_not be_nil
-    end
-    
-    #.........................................................................................................
-    should "on receipt of first presence message from jid and if jid is in configured roster, create presence status entry in configured roster item record for resource and send client version request to jid" do
-      first_presence
+    should "create presence status entry in roster item record for resource and send client version request to jid" do
     end
       
     #.........................................................................................................
-    should "update presence status for an existing resource entry for roster item with jid in the configured roster" do
-      first_presence
+    should "update roster item resource presence status on receiving subsequent presence messages" do
       @delegate = @client.new_delegate
       @delegate.did_receive_presence_method.should_not be_called
       @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'][:presence].type.should be_nil # nil presence type=available
@@ -50,8 +39,7 @@ class TestPresenceManagement < Test::Unit::TestCase
     end
      
     #.........................................................................................................
-    should "maintain multiple presence status entries for a roster item with jid in configured roster" do
-      first_presence
+    should "maintain multiple presence status entries for multiple resources for a roster item" do
       @delegate = @client.new_delegate
       @delegate.did_receive_presence_method.should_not be_called
       @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/work'].should be_nil
@@ -60,38 +48,47 @@ class TestPresenceManagement < Test::Unit::TestCase
       @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/work'][:presence].should_not be_nil  
       @client.roster['troy@nowhere.com'][:resources]['troy@nowhere.com/home'][:presence].should_not be_nil  
     end
-     
-    #.........................................................................................................
-    should "ignore presence messages from jids not in configured roster" do
-      @client.roster.has_key?('noone@nowhere.com').should be(false)
-      @delegate.did_receive_presence_method.should_not be_called
-      @client.receiving(PresenceMessages.recv_presence_available(@client, 'noone@nowhere.com/here')).should not_respond
-      @delegate.did_receive_presence_method.should be_called
-      @client.roster.has_key?('noone@nowhere.com').should be(false)
-    end
-      
-    #.........................................................................................................
-    should "accept subscription requests from jids which are in the configured roster" do
-      @client.roster.has_key?('troy@nowhere.com').should be(true)
-      @delegate.did_receive_subscribe_request_method.should_not be_called
-      @client.receiving(PresenceMessages.recv_presence_subscribe(@client, 'troy@nowhere.com')).should \
-        respond_with(PresenceMessages.send_presence_subscribed(@client, 'troy@nowhere.com'))
-      @client.receiving(PresenceMessages.recv_presence_subscribed(@client, 'troy@nowhere.com')).should not_respond
-      @delegate.did_receive_subscribe_request_method.should be_called
-      @client.roster.has_key?('troy@nowhere.com').should be(true)
-    end
-    
-    #.........................................................................................................
-    should "decline subscription requests from jids which are not in the configured roster" do
-      @client.roster.has_key?('noone@nowhere.com').should be(false)
-      @delegate.did_receive_subscribe_request_method.should_not be_called
-      @client.receiving(PresenceMessages.recv_presence_subscribe(@client, 'noone@nowhere.com')).should \
-        respond_with(PresenceMessages.send_presence_unsubscribed(@client, 'noone@nowhere.com'))
-      @delegate.did_receive_subscribe_request_method.should be_called
-      @client.roster.has_key?('noone@nowhere.com').should be(false)
-    end
-    
-  end
 
+  end
+     
+  #.........................................................................................................
+  should "create presence status for resource on receipt of self presence" do
+    @client.roster[@client.client.jid.bare.to_s][:resources].should be_empty
+    @delegate.did_receive_presence_method.should_not be_called
+    @client.receiving(PresenceMessages.recv_presence_self(@client)).should not_respond
+    @delegate.did_receive_presence_method.should be_called
+    @client.roster[@client.client.jid.bare.to_s][:resources][@client.client.jid.to_s][:presence].should_not be_nil
+  end
+       
+  #.........................................................................................................
+  should "ignore presence messages from jids not in configured roster" do
+    @client.roster.has_key?('noone@nowhere.com').should be(false)
+    @delegate.did_receive_presence_method.should_not be_called
+    @client.receiving(PresenceMessages.recv_presence_available(@client, 'noone@nowhere.com/here')).should not_respond
+    @delegate.did_receive_presence_method.should be_called
+    @client.roster.has_key?('noone@nowhere.com').should be(false)
+  end
+    
+  #.........................................................................................................
+  should "accept subscription requests from jids which are in the configured roster" do
+    @client.roster.has_key?('troy@nowhere.com').should be(true)
+    @delegate.did_receive_subscribe_request_method.should_not be_called
+    @client.receiving(PresenceMessages.recv_presence_subscribe(@client, 'troy@nowhere.com')).should \
+      respond_with(PresenceMessages.send_presence_subscribed(@client, 'troy@nowhere.com'))
+    @client.receiving(PresenceMessages.recv_presence_subscribed(@client, 'troy@nowhere.com')).should not_respond
+    @delegate.did_receive_subscribe_request_method.should be_called
+    @client.roster.has_key?('troy@nowhere.com').should be(true)
+  end
+  
+  #.........................................................................................................
+  should "decline subscription requests from jids which are not in the configured roster" do
+    @client.roster.has_key?('noone@nowhere.com').should be(false)
+    @delegate.did_receive_subscribe_request_method.should_not be_called
+    @client.receiving(PresenceMessages.recv_presence_subscribe(@client, 'noone@nowhere.com')).should \
+      respond_with(PresenceMessages.send_presence_unsubscribed(@client, 'noone@nowhere.com'))
+    @delegate.did_receive_subscribe_request_method.should be_called
+    @client.roster.has_key?('noone@nowhere.com').should be(false)
+  end
+  
 end
 
