0839bcfd0d553648c326fa228f1b268ece71b707
Troy Stribling <troy.stribling@gmail.com>
2009-06-27 15:21:17 -0400

    moved all message processing logic to message_pipe

--- lib/agent_xmpp/client/client.rb
+++ lib/agent_xmpp/client/client.rb
@@ -5,7 +5,7 @@ module AgentXmpp
   class Client
 
     #---------------------------------------------------------------------------------------------------------
-    attr_reader :port, :password, :roster, :connection, :pipe
+    attr_reader :port, :password, :connection, :pipe
     attr_accessor :jid
     #---------------------------------------------------------------------------------------------------------
 
@@ -15,9 +15,7 @@ module AgentXmpp
       @port = config['port'] || 5222
       resource = config['resource'] || Socket.gethostname
       @jid = Xmpp::JID.new("#{config['jid']}/#{resource}")
-      @roster = RosterModel.new(@jid, config['contacts'])
-      @pipe = MessagePipe.new(self)
-      pipe.add_delegate(self) 
+      @pipe = MessagePipe.new(self, config)
     end
 
     #.........................................................................................................
@@ -60,181 +58,90 @@ module AgentXmpp
     # connection
     #.........................................................................................................
     def did_connect(pipe)
-      AgentXmpp.logger.info "CONNECTED"
     end
 
     #.........................................................................................................
     def did_disconnect(pipe)
-      AgentXmpp.logger.warn "DISCONNECTED"
-      EventMachine::stop_event_loop
     end
 
     #.........................................................................................................
     def did_not_connect(pipe)
-      AgentXmpp.logger.warn "CONNECTION FAILED"
     end
 
     #.........................................................................................................
     # authentication
     #.........................................................................................................
     def did_authenticate(pipe, stanza)
-      AgentXmpp.logger.info "AUTHENTICATED"
     end
  
     #.........................................................................................................
     def did_not_authenticate(pipe, stanza)
-      AgentXmpp.logger.info "AUTHENTICATION FAILED"
     end
 
     #.........................................................................................................
     def did_bind(pipe, stanza)
-      AgentXmpp.logger.info "BIND ACKNOWLEDGED"
     end
 
     #.........................................................................................................
     def did_start_session(pipe, stanza)
-      AgentXmpp.logger.info "SESSION STARTED"
-      Xmpp::IqRoster.get(pipe)
     end
 
     #.........................................................................................................
     # presence
     #.........................................................................................................
     def did_receive_presence(pipe, presence)
-      from_jid = presence.from.to_s     
-      from_bare_jid = presence.from.bare.to_s     
-      if roster.has_key?(from_bare_jid) 
-        roster[from_bare_jid.to_s][:resources][from_jid] = {} if roster[from_bare_jid.to_s][:resources][from_jid].nil?
-        roster[from_bare_jid.to_s][:resources][from_jid][:presence] = presence
-        AgentXmpp.logger.info "RECEIVED PRESENCE FROM: #{from_jid}"
-        Xmpp::IqVersion.request(from_jid, pipe) if not from_jid.eql?(pipe.jid.to_s) and presence.type.nil?
-      else
-        AgentXmpp.logger.warn "RECEIVED PRESENCE FROM JID NOT IN ROSTER: #{from_jid}" 
-      end
     end
 
     #.........................................................................................................
     def did_receive_presence_subscribe(pipe, presence)
-      from_jid = presence.from.to_s     
-      if roster.has_key?(presence.from.bare.to_s ) 
-        AgentXmpp.logger.info "RECEIVED SUBSCRIBE REQUEST: #{from_jid}"
-        Xmpp::Presence.accept(from_jid)  
-      else
-        AgentXmpp.logger.warn "RECEIVED SUBSCRIBE REQUEST FROM JID NOT IN ROSTER: #{from_jid}"        
-        Xmpp::Presence.decline(from_jid)  
-      end
     end
 
     #.........................................................................................................
     def did_receive_presence_unsubscribed(pipe, presence)
-      from_jid = presence.from.to_s     
-      if roster.delete(presence.from.bare.to_s )           
-        AgentXmpp.logger.info "RECEIVED UNSUBSCRIBED REQUEST: #{from_jid}"
-        Xmpp::IqRoster.remove(presence.from, pipe)  
-      else
-        AgentXmpp.logger.warn "RECEIVED UNSUBSCRIBED REQUEST FROM JID NOT IN ROSTER: #{from_jid}"   
-      end
     end
 
     #.........................................................................................................
     def did_receive_presence_subscribed(pipe, presence)
-      from_jid = presence.from.to_s     
-      AgentXmpp.logger.warn "SUBSCRIPTION ACCEPTED: #{from_jid}" 
     end
 
     #.........................................................................................................
     # roster management
     #.........................................................................................................
     def did_receive_roster_item(pipe, roster_item)
-      AgentXmpp.logger.info "RECEIVED ROSTER ITEM"   
-      roster_item_jid = roster_item.jid.to_s
-      if roster.has_key?(roster_item_jid) 
-        case roster_item.subscription   
-        when :none
-          if roster_item.ask.eql?(:subscribe)
-            AgentXmpp.logger.info "CONTACT SUBSCRIPTION PENDING: #{roster_item_jid}"   
-            roster[roster_item_jid][:status] = :ask 
-          else
-            AgentXmpp.logger.info "CONTACT ADDED TO ROSTER: #{roster_item_jid}"   
-            roster[roster_item_jid][:status] = :added 
-          end
-        when :to
-          AgentXmpp.logger.info "SUBSCRIBED TO CONTACT PRESENCE: #{roster_item_jid}"   
-          roster[roster_item_jid][:status] = :to 
-        when :from
-          AgentXmpp.logger.info "CONTACT SUBSCRIBED TO PRESENCE: #{roster_item_jid}"   
-          roster[roster_item_jid][:status] = :from 
-        when :both    
-          AgentXmpp.logger.info "CONTACT SUBSCRIPTION BIDIRECTIONAL: #{roster_item_jid}"   
-          roster[roster_item_jid][:status] = :both 
-          roster[roster_item_jid][:roster_item] = roster_item 
-        end
-      else
-        AgentXmpp.logger.info "REMOVING ROSTER ITEM: #{roster_item_jid}"   
-        Xmpp::IqRoster.remove(roster_item.jid, pipe)  
-      end
     end
 
     #.........................................................................................................
     def did_remove_roster_item(pipe, roster_item)
-      AgentXmpp.logger.info "REMOVE ROSTER ITEM"   
-      roster_item_jid = roster_item.jid.to_s
-      if roster.has_key?(roster_item_jid) 
-        AgentXmpp.logger.info "REMOVED ROSTER ITEM: #{roster_item_jid}"   
-        roster.delete(roster_item_jid) 
-      end
     end
 
     #.........................................................................................................
     def did_receive_all_roster_items(pipe)
-      AgentXmpp.logger.info "RECEIVED ALL ROSTER ITEMS"   
-      roster.select{|j,r| r[:status].eql?(:inactive)}.collect do |j, r|
-        AgentXmpp.logger.info "ADDING CONTACT: #{j}" 
-        Xmpp::IqRoster.add(Xmpp::JID.new(j), pipe)  
-      end
     end
 
     #.........................................................................................................
     def did_acknowledge_add_roster_item(pipe, response, roster_item_jid)
-      AgentXmpp.logger.info "ADD ROSTER ITEM ACKNOWLEDGED: #{roster_item_jid.to_s}"
     end
 
     #.........................................................................................................
     def did_acknowledge_remove_roster_item(pipe, response, roster_item_jid)
-      AgentXmpp.logger.info "REMOVE ROSTER ITEM ACKNOWLEDGED: #{roster_item_jid.to_s}"
     end
 
     #.........................................................................................................
     def did_receive_remove_roster_item_error(pipe, response, roster_item_jid)
-      AgentXmpp.logger.info "REMOVE ROSTER ITEM RECEIVED ERROR: #{roster_item_jid.to_s}"
     end
 
     #.........................................................................................................
     def did_receive_add_roster_item_error(pipe, response, roster_item_jid)
-      AgentXmpp.logger.info "ADD ROSTER ITEM RECEIVED ERROR REMOVING: #{roster_item_jid.to_s}"
-      roster.delete(roster_item_jid)
     end
 
     #.........................................................................................................
     # service discovery management
     #.........................................................................................................
     def did_receive_client_version_result(pipe, from, version)
-      if roster.has_key?(from.bare.to_s)
-        AgentXmpp.logger.info "RECEIVED CLIENT VERSION RESULT: #{from.to_s}, #{version.iname}, #{version.version}"
-        roster[from.bare.to_s][:resources][from.to_s][:version] = version
-      else
-        AgentXmpp.logger.warn "RECEIVED CLIENT VERSION RESULT FROM JID NOT IN ROSTER: #{from.to_s}"
-      end        
     end
 
     #.........................................................................................................
     def did_receive_client_version_get(pipe, request)
-      if roster.has_key?(request.from.bare.to_s)
-        AgentXmpp.logger.info "RECEIVED CLIENT VERSION REQUEST: #{request.from.to_s}"
-        Xmpp::IqVersion.respond(request, pipe)
-      else
-        AgentXmpp.logger.warn "RECEIVED CLIENT VERSION REQUEST FROM JID NOT IN ROSTER: #{request.from.to_s}"
-      end
     end
 
   #### Client
--- lib/agent_xmpp/client/message_pipe.rb
+++ lib/agent_xmpp/client/message_pipe.rb
@@ -5,19 +5,21 @@ module AgentXmpp
   class MessagePipe
 
     #---------------------------------------------------------------------------------------------------------
-    attr_reader   :connection_status, :delegates, :id_callbacks, :client, :stream_features, :stream_mechanisms             
+    attr_reader   :connection_status, :delegates, :id_callbacks, :client, :stream_features, \
+                  :stream_mechanisms, :roster            
     attr_accessor :connection               
     #---------------------------------------------------------------------------------------------------------
     alias_method :send_to_method, :send
     #---------------------------------------------------------------------------------------------------------
 
     #.........................................................................................................
-    def initialize(client)
+    def initialize(client, config)
       @client = client
       @connection_status = :offline;
-      @delegates = []
+      @delegates = [self.class]
       @connection = nil
       @id_callbacks = {}
+      @roster = RosterModel.new(client.jid, config['contacts'])
     end
     
     #.........................................................................................................
@@ -204,6 +206,167 @@ module AgentXmpp
       msg.push(Send("<stream:stream xmlns='jabber:client' xmlns:stream='http://etherx.jabber.org/streams' version='1.0' to='#{jid.domain}'>"))
     end
           
+  public
+    
+    #####-------------------------------------------------------------------------------------------------------
+    class << self
+      
+      #---------------------------------------------------------------------------------------------------------
+      # event flow delegate methods
+      #.........................................................................................................
+      # connection
+      #.........................................................................................................
+      def did_connect(pipe)
+        AgentXmpp.logger.info "CONNECTED"
+      end
+
+      #.........................................................................................................
+      def did_disconnect(pipe)
+        AgentXmpp.logger.warn "DISCONNECTED"
+        EventMachine::stop_event_loop
+      end
+
+      #.........................................................................................................
+      def did_not_connect(pipe)
+        AgentXmpp.logger.warn "CONNECTION FAILED"
+      end
+
+      #.........................................................................................................
+      # authentication
+      #.........................................................................................................
+      def did_start_session(pipe, stanza)
+        AgentXmpp.logger.info "SESSION STARTED"
+        Xmpp::IqRoster.get(pipe)
+      end
+
+      #.........................................................................................................
+      # presence
+      #.........................................................................................................
+      def did_receive_presence(pipe, presence)
+        from_jid = presence.from.to_s     
+        from_bare_jid = presence.from.bare.to_s     
+        if pipe.roster.has_key?(from_bare_jid) 
+          pipe.roster[from_bare_jid.to_s][:resources][from_jid] = {} if pipe.roster[from_bare_jid.to_s][:resources][from_jid].nil?
+          pipe.roster[from_bare_jid.to_s][:resources][from_jid][:presence] = presence
+          AgentXmpp.logger.info "RECEIVED PRESENCE FROM: #{from_jid}"
+          Xmpp::IqVersion.request(from_jid, pipe) if not from_jid.eql?(pipe.jid.to_s) and presence.type.nil?
+        else
+          AgentXmpp.logger.warn "RECEIVED PRESENCE FROM JID NOT IN ROSTER: #{from_jid}" 
+        end
+      end
+
+      #.........................................................................................................
+      def did_receive_presence_subscribe(pipe, presence)
+        from_jid = presence.from.to_s     
+        if pipe.roster.has_key?(presence.from.bare.to_s ) 
+          AgentXmpp.logger.info "RECEIVED SUBSCRIBE REQUEST: #{from_jid}"
+          Xmpp::Presence.accept(from_jid)  
+        else
+          AgentXmpp.logger.warn "RECEIVED SUBSCRIBE REQUEST FROM JID NOT IN ROSTER: #{from_jid}"        
+          Xmpp::Presence.decline(from_jid)  
+        end
+      end
+
+      #.........................................................................................................
+      def did_receive_presence_unsubscribed(pipe, presence)
+        from_jid = presence.from.to_s     
+        if pipe.roster.delete(presence.from.bare.to_s )           
+          AgentXmpp.logger.info "RECEIVED UNSUBSCRIBED REQUEST: #{from_jid}"
+          Xmpp::IqRoster.remove(presence.from, pipe)  
+        else
+          AgentXmpp.logger.warn "RECEIVED UNSUBSCRIBED REQUEST FROM JID NOT IN ROSTER: #{from_jid}"   
+        end
+      end
+
+      #.........................................................................................................
+      def did_receive_presence_subscribed(pipe, presence)
+        from_jid = presence.from.to_s     
+        AgentXmpp.logger.warn "SUBSCRIPTION ACCEPTED: #{from_jid}" 
+      end
+
+      #.........................................................................................................
+      # roster management
+      #.........................................................................................................
+      def did_receive_roster_item(pipe, roster_item)
+        AgentXmpp.logger.info "RECEIVED ROSTER ITEM"   
+        roster_item_jid = roster_item.jid.to_s
+        if pipe.roster.has_key?(roster_item_jid) 
+          case roster_item.subscription   
+          when :none
+            if roster_item.ask.eql?(:subscribe)
+              AgentXmpp.logger.info "CONTACT SUBSCRIPTION PENDING: #{roster_item_jid}"   
+              pipe.roster[roster_item_jid][:status] = :ask 
+            else
+              AgentXmpp.logger.info "CONTACT ADDED TO ROSTER: #{roster_item_jid}"   
+              pipe.roster[roster_item_jid][:status] = :added 
+            end
+          when :to
+            AgentXmpp.logger.info "SUBSCRIBED TO CONTACT PRESENCE: #{roster_item_jid}"   
+            pipe.roster[roster_item_jid][:status] = :to 
+          when :from
+            AgentXmpp.logger.info "CONTACT SUBSCRIBED TO PRESENCE: #{roster_item_jid}"   
+            pipe.roster[roster_item_jid][:status] = :from 
+          when :both    
+            AgentXmpp.logger.info "CONTACT SUBSCRIPTION BIDIRECTIONAL: #{roster_item_jid}"   
+            pipe.roster[roster_item_jid][:status] = :both 
+            pipe.roster[roster_item_jid][:roster_item] = roster_item 
+          end
+        else
+          AgentXmpp.logger.info "REMOVING ROSTER ITEM: #{roster_item_jid}"   
+          Xmpp::IqRoster.remove(roster_item.jid, pipe)  
+        end
+      end
+
+      #.........................................................................................................
+      def did_remove_roster_item(pipe, roster_item)
+        AgentXmpp.logger.info "REMOVE ROSTER ITEM"   
+        roster_item_jid = roster_item.jid.to_s
+        if pipe.roster.has_key?(roster_item_jid) 
+          AgentXmpp.logger.info "REMOVED ROSTER ITEM: #{roster_item_jid}"   
+          pipe.roster.delete(roster_item_jid) 
+        end
+      end
+
+      #.........................................................................................................
+      def did_receive_all_roster_items(pipe)
+        AgentXmpp.logger.info "RECEIVED ALL ROSTER ITEMS"   
+        pipe.roster.select{|j,r| r[:status].eql?(:inactive)}.collect do |j, r|
+          AgentXmpp.logger.info "ADDING CONTACT: #{j}" 
+          Xmpp::IqRoster.add(Xmpp::JID.new(j), pipe)  
+        end
+      end
+
+      #.........................................................................................................
+      def did_receive_add_roster_item_error(pipe, response, roster_item_jid)
+        AgentXmpp.logger.info "ADD ROSTER ITEM RECEIVED ERROR REMOVING: #{roster_item_jid.to_s}"
+        pipe.roster.delete(roster_item_jid)
+      end
+
+      #.........................................................................................................
+      # service discovery management
+      #.........................................................................................................
+      def did_receive_client_version_result(pipe, from, version)
+        if pipe.roster.has_key?(from.bare.to_s)
+          AgentXmpp.logger.info "RECEIVED CLIENT VERSION RESULT: #{from.to_s}, #{version.iname}, #{version.version}"
+          pipe.roster[from.bare.to_s][:resources][from.to_s][:version] = version
+        else
+          AgentXmpp.logger.warn "RECEIVED CLIENT VERSION RESULT FROM JID NOT IN ROSTER: #{from.to_s}"
+        end        
+      end
+
+      #.........................................................................................................
+      def did_receive_client_version_get(pipe, request)
+        if pipe.roster.has_key?(request.from.bare.to_s)
+          AgentXmpp.logger.info "RECEIVED CLIENT VERSION REQUEST: #{request.from.to_s}"
+          Xmpp::IqVersion.respond(request, pipe)
+        else
+          AgentXmpp.logger.warn "RECEIVED CLIENT VERSION REQUEST FROM JID NOT IN ROSTER: #{request.from.to_s}"
+        end
+      end
+         
+    #### self
+    end
+     
   #### MessagePipe
   end
 
